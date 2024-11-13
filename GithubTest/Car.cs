0e748f14384cb0ffa9bf571da06e8b8c0f248943
Thomas Larsen <thl001@edu.zealand.dk>
2024-11-13 13:19:02 +0100

    First push

---
+++ GithubTest/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubTest
+{
+    public class Car
+    {
+        public string NumberPlate { get; set; }
+        public string Brand { get; set; }
+
+        public Car(string numberPlate, string brand)
+        {
+            NumberPlate = numberPlate;
+            Brand = brand;
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("wroom wroom");
+        }
+
+    }
+}
--- GithubTest/Program.cs
+++ GithubTest/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GithubTest;
+
+Car car = new Car("ab12345", "mercedes");
